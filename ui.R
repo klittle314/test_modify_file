642b9ad94243602dc3e8aee52bb72bb4c9d4d341
klittle314 <klittle@iecodesign.com>
2016-06-30 12:25:04 -0500

    broke up the shinyapp into ui and server modules for ease of editing.   This is the ui module.

---
+++ ui.R
@@ -0,0 +1,29 @@
+
+
+  shinyUI(fluidPage(
+    titlePanel("NNOHA Collaborative Data Page"),
+    
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("choose_clinic", label = h3("Select clinic"), 
+                    choices = clinic_list, 
+                    selected = 1),
+        
+        br(),
+        
+        fileInput('file1', 'Upload Clinic Data Template',
+                  accept=c('.xlsx','.xls')), 
+      
+        #drop down to select the Measure
+        htmlOutput("selectMeasures"),
+        
+        #drop down to select the Team
+        htmlOutput("selectTeam")
+      #actionButton("Update1", "Update the data file"),
+      #actionButton("Update2", "Update display")
+      ),
+    mainPanel(
+      dataTableOutput("df_data_out")
+    )
+  )))
+  
