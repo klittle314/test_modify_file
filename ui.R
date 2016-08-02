fc065c3c25fb7bec6ac47fa70971709e45ef3942
klittle314 <klittle@iecodesign.com>
2016-08-02 14:42:02 -0500

    repair ( and , within tabPanel sequence

--- ui.R
+++ ui.R
@@ -60,8 +60,8 @@ shinyUI(navbarPage("NNOHA Collaborative Data Web Application",
                   
                   tabPanel("Measure by Health Center",
                            plotOutput("measure_plot",height="800px")),
-                  tabPanel)"Health Center Measures",
-                           plotOutput("")
+                  tabPanel("Health Center Measures",
+                           plotOutput(""),
                   tabPanel("Data Table",dataTableOutput("df_data_out"))
       )
      )
