e3ceac16abe33b915b856a5777e3e9c86b1231d2
klittle314 <klittle@iecodesign.com>
2016-08-05 13:13:49 -0500

    revise to plot output of team_plot

--- ui.R
+++ ui.R
@@ -61,8 +61,8 @@ shinyUI(navbarPage("NNOHA Collaborative Data Web Application",
                   tabPanel("Measure by Health Center",
                            plotOutput("measure_plot",height="800px")),
                   #to uncomment when I have the function working
-                  tabPanel("Health Center Measures"),
-                           #plotOutput("")),
+                  tabPanel("Health Center Measures",
+                           plotOutput("team_plot", height="800px")),
                   tabPanel("Data Table",dataTableOutput("df_data_out"))
       )
      )
