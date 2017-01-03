8a90c115716bfe71117d7daadfed2ea51f633014
klittle314 <klittle@iecodesign.com>
2017-01-03 10:59:38 -0600

    change to test google sheet

--- global.R
+++ global.R
@@ -1,5 +1,5 @@
 #shiny app allows users to load data from an Excel template to update a master Google sheet
-# Kevin Little, Ph.D.  Informing Ecological Design, LLC  June-Nov 2016
+# Kevin Little, Ph.D.  Informing Ecological Design, LLC  June-Dec 2016
 
 library(Rcpp)
 library(ggplot2)
@@ -33,10 +33,10 @@ measname_table <- read.xlsx(path2,sheet="lookup")
 
 #load the googlesheet
 #test sheet to avoid contaminating the master data table: TEST_NNOHA Dental Dashboard Tracking Tool 30 Aug 2016
-#gskey2 <- c("1moCxJ0bw5D0B3OqZ-iqJhN8MUMyhiwJx9U5_aWPQYlo")
+gskey2 <- c("1moCxJ0bw5D0B3OqZ-iqJhN8MUMyhiwJx9U5_aWPQYlo")
 
 #working sheet NNOHA Dental Dashboard Tracking Tool 30 Aug 2016:  take the coded from the URL of the sheet
-gskey2 <- c("1_iIVDAS1gzqr6KYcYZiNicE3o7anPJNXQJTWcrLIfW4")
+#gskey2 <- c("1_iIVDAS1gzqr6KYcYZiNicE3o7anPJNXQJTWcrLIfW4")
 gsobj <- gs_key(x=gskey2)
 
 #retrieve google sheet data, force as data.frame else the object is a tbl that seems to confuse other
